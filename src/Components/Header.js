f34f8a3a3c4079198f500f9c49bbab5a9aa21ac0
jbmkahdeksan <45461757+jbmkahdeksan@users.noreply.github.com>
2021-08-22 21:10:59 -0600

    project built

---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+import Button from  './Button'
+import { useLocation } from 'react-router-dom'
+
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            {location.pathname === '/' && 
+            <Button 
+                onClick={onAdd} 
+                text={showAdd ? 'Close' : 'Add'}
+                color={showAdd ? 'red' : 'green'}
+            />}
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired ,
+}
+
+// CSS in JS 
+// const HeadingStyle = {color: 'red', backgroundColor:'black'}
+
+export default Header
